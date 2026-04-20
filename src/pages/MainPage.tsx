10dccfd3418a2af62d99e7178424d4863a53ee96
daaoooy <y020d226y@daum.net>
2026-04-21 04:15:11 +0900

    ✨ Update MainPage to include buttons for color token testing and improve layout

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import ThemeToggle from "@/features/theme/components/ThemeToggle";
 import { useTheme } from "@/features/theme/hooks/useTheme";
+import { Button } from "@/shared/components/shadcn/button";
 
 const MainPage = () => {
   const { dark, toggleTheme } = useTheme();
@@ -9,7 +10,17 @@ const MainPage = () => {
       <div className="h-10 w-full flex justify-start p-1.5 shrink-0">
         <ThemeToggle dark={dark} toggleTheme={toggleTheme} />
       </div>
-      <div className="h-full flex items-center">테스트 중입니다.</div>
+      <div className="h-full flex items-center">
+        <p className="pr-10">테스트 중</p>
+        <div className="flex flex-row gap-2">
+          <Button className="bg-red-cross-red-100 hover:bg-red-cross-red-80 text-white">
+            색상 토큰 테스트 1
+          </Button>
+          <Button className="bg-red-cross-warm-gray-100 hover:bg-red-cross-wram-gray-80 text-white">
+            색상 토큰 테스트 2
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
