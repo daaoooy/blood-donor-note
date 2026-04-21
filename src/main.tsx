6f6126c54ec9eb9a5e74a34262f99d3f7ef7ce4f
daaoooy <y020d226y@daum.net>
2026-04-21 18:54:55 +0900

    ✨ (#9) Refactor to include QueryClientProvider and ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import '@/styles/design-tokens/index.css';
+
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+
+import { queryClient } from '@/lib/react-query.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
