3eff0e51013e757be9368f57912cc8a0d095af6c
daaoooy <y020d226y@daum.net>
2026-04-24 16:37:27 +0900

    ✨ Add new pages for donation, map, quiz, and settings: implement BackTopTab component in each page

---
+++ src/pages/AddDonation.tsx
@@ -0,0 +1,12 @@
+import BackTopTab from '@/shared/components/BackTopTab';
+
+const AddDonation = () => {
+  return (
+    <div>
+      <BackTopTab />
+      헌혈 이력 등록 페이지
+    </div>
+  );
+};
+
+export default AddDonation;
---
+++ src/pages/Map.tsx
@@ -0,0 +1,12 @@
+import BackTopTab from '@/shared/components/BackTopTab';
+
+const Map = () => {
+  return (
+    <div>
+      <BackTopTab />
+      헌혈의 집 찾기 페이지
+    </div>
+  );
+};
+
+export default Map;
